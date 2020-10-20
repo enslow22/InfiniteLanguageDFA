ff09df7d358d3f789026c27b9c8734fac031fa3b
Ryan <ryan.enslow@gmail.com>
2020-10-20 12:16:07 -0700

    Added Comments to methods that needed them

--- InfiniteLanguageDFA/Program.cs
+++ InfiniteLanguageDFA/Program.cs
@@ -5,23 +5,26 @@ namespace InfiniteLanguageDFA
 {
     class Program
     {
-
+        
         Node[] states;
         static void Main(string[] args)
         {
             //Execute from main. Main will take in a file idk how yet
         }
 
-        public Node[] DFAComplement()
+        //Returns the complement of a given DFA
+        public Node[] DFAComplement(Node[] n)
         {
             return null;
         }
 
+        //Executes a DFS on a DFA
         public bool DFS(Node[] n)
         {
             return false;
         }
 
+        //Parses a .txt file and returns the DFA encoded in it
         public Node[] ParseFile(FileStream f)
         {
             return null;
