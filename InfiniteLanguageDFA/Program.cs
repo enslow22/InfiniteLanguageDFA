2dbd2d210fe47a448a0f57369228dc9dc405577f
Ryan <ryan.enslow@gmail.com>
2020-10-20 11:39:11 -0700

    Initial Commit

---
+++ InfiniteLanguageDFA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InfiniteLanguageDFA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
