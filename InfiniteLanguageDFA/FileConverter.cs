222c153c52b881133a3964bf44c8a2a241d81728
Ryan <enslow22@up.edu>
2020-11-22 23:20:56 -0800

    Created a new class to convert .txt files into DFAs

---
+++ InfiniteLanguageDFA/FileConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfiniteLanguageDFA
+{
+    /*
+     * This class converts .txt files into Node[] objects.
+     */
+    class FileConverter
+    {
+
+    }
+}
